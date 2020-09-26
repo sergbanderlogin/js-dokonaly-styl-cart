9fbdf3390fad30199fcc3a3ea12a2dde66bd9fff
sergbanderlogin <sergbanderlogin@gmail.com>
2020-09-26 14:33:10 +0300

    add file .jshintrc

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -7,7 +7,7 @@ userRouter.get("/createadmin", async (req, res) => {
   try {
     const user = new User({
       name: 'admin',
-      email: 'bunakov.sergey@gmil.com',
+      email: 'admin@example.com',
       password: 'jsdokonalystyl',
       isAdmin: true,
     });
